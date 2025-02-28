1325cf6aa2d1831d205ca1a29ad0685c475a04ba
aurianebgnl <auriane.bgnl@gmail.com>
2025-02-28 11:51:32 +0100

    étape 5 - affichage new citation

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Sélection des éléments HTML
 const form = document.querySelector('#quote-form');
 const submitButton = document.querySelector('.submit-btn');
+const quoteList = document.querySelector('#quote-list');
 
 submitButton.addEventListener('click', (event) => {
     const inputText = document.querySelector('#quote').value;
@@ -10,6 +11,27 @@ submitButton.addEventListener('click', (event) => {
     
     console.log('Valeur du champ text:', inputText);
     console.log('Valeur du champ auteur:', inputAuthor);
+    addQuote(inputText, inputAuthor);
     
 });
 
+function addQuote(inputText, inputAuthor) {
+    
+    const newText = document.createElement("p");
+    newText.className = "text";
+    newText.innerText = inputText;
+    // autre possibilité testée à la place de .innerText (marche aussi) : 
+        // const textNode = document.createTextNode(inputText);
+        // newText.appendChild(textNode);
+    
+    const newAuthor = document.createElement("p");
+    newAuthor.className = "author";
+    newAuthor.innerText = inputAuthor;
+
+    const newDiv = document.createElement("div"); 
+    newDiv.className = "quote";
+    newDiv.appendChild(newText);
+    newDiv.appendChild(newAuthor);
+
+    quoteList.appendChild(newDiv);
+}
